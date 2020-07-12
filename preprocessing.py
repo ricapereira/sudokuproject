c10e2a2afbd864ff410b0ff02f613d57a4111d51
Ricardo <rica.pereiraa@gmail.com>
2020-07-11 23:01:39 -0300

    Não deu

--- preprocessing.py
+++ preprocessing.py
@@ -4,9 +4,8 @@ import keras as k
 import numpy as np
 import operator
 
-path = r'images\\test1.jpg'
+path = r'images\\test3.jpg'
 img0 = cv2.imread(path)
-print(img0.shape[0])
 if (img0.shape[0]<600) or (img0.shape[1]<600):
     img0 = cv2.resize(img0,(600,600))
 cv2.imshow('original', img0)
@@ -20,7 +19,7 @@ def preprocess_img(img):
     #Transform to inverse binary image
     img = cv2.adaptiveThreshold(dst, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 3)
     #Dilate the boundaries
-    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(2,2))
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
     img = cv2.dilate(img,kernel,iterations = 1)
 
     cv2.imshow('dilate', img)
@@ -79,17 +78,92 @@ warp = cut_and_warp(img, corners)
 warp = cv2.GaussianBlur(warp,(3,3),3)
 cv2.imshow('final', warp)
 cv2.waitKey(0)
-
+'''
 def houghtransf(warp):
-    lines = cv2.HoughLinesP(warp,2,np.pi/2,500,minLineLength=200,maxLineGap=10)
+    lines = cv2.HoughLinesP(warp,2,np.pi/2,500,minLineLength=450,maxLineGap=10)
     frame = cv2.cvtColor(warp, cv2.COLOR_GRAY2RGB)
+    print(lines.shape)
     for l in lines:
         x1,y1,x2,y2 = l[0]
         cv2.line(frame,(x1,y1),(x2,y2),(0,0,255),2, cv2.LINE_AA)
     return frame
-    
+ 
 
 lines = houghtransf(warp)
 cv2.imshow('lines', lines)
 cv2.waitKey(0)
-        
+'''        
+edges = cv2.Canny(warp, 90, 150, apertureSize = 3)
+frame = cv2.cvtColor(warp, cv2.COLOR_GRAY2RGB)
+
+lines = cv2.HoughLines(edges,1,np.pi/180,150)
+
+if not lines.any():
+    print('No lines were found')
+    exit()
+
+if filter:
+    rho_threshold = 15
+    theta_threshold = 0.1
+
+    # how many lines are similar to a given one
+    similar_lines = {i : [] for i in range(len(lines))}
+    for i in range(len(lines)):
+        for j in range(len(lines)):
+            if i == j:
+                continue
+
+            rho_i,theta_i = lines[i][0]
+            rho_j,theta_j = lines[j][0]
+            if abs(rho_i - rho_j) < rho_threshold and abs(theta_i - theta_j) < theta_threshold:
+                similar_lines[i].append(j)
+
+    # ordering the INDECES of the lines by how many are similar to them
+    indices = [i for i in range(len(lines))]
+    indices.sort(key=lambda x : len(similar_lines[x]))
+
+    # line flags is the base for the filtering
+    line_flags = len(lines)*[True]
+    for i in range(len(lines) - 1):
+        if not line_flags[indices[i]]: # if we already disregarded the ith element in the ordered list then we don't care (we will not delete anything based on it and we will never reconsider using this line again)
+            continue
+
+        for j in range(i + 1, len(lines)): # we are only considering those elements that had less similar line
+            if not line_flags[indices[j]]: # and only if we have not disregarded them already
+                continue
+
+            rho_i,theta_i = lines[indices[i]][0]
+            rho_j,theta_j = lines[indices[j]][0]
+            if abs(rho_i - rho_j) < rho_threshold and abs(theta_i - theta_j) < theta_threshold:
+                line_flags[indices[j]] = False # if it is similar and have not been disregarded yet then drop it now
+
+print('number of Hough lines:', len(lines))
+
+filtered_lines = []
+
+if filter:
+    for i in range(len(lines)): # filtering
+        if line_flags[i]:
+            filtered_lines.append(lines[i])
+
+    print('Number of filtered lines:', len(filtered_lines))
+else:
+    filtered_lines = lines
+
+for line in filtered_lines:
+    rho,theta = line[0]
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a*rho
+    y0 = b*rho
+    x1 = int(x0 + 1000*(-b))
+    y1 = int(y0 + 1000*(a))
+    x2 = int(x0 - 1000*(-b))
+    y2 = int(y0 - 1000*(a))
+
+    cv2.line(frame,(x1,y1),(x2,y2),(0,0,255),2)
+
+
+cv2.imshow('lines', frame)
+cv2.waitKey(0)
+
